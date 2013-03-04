1f6567e1030f0df99db58273b75c1ef8a517cd56
mies <mies@wercker.com>
2013-03-04 15:48:53 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'd3a75318c1aec40b35cea81bad5cb9d73167914e4665b3e44eb70f5d38e2bb91e205ddd71178fd7234e9d41c13f4923c186b2e41e8bc43aba5656a5302034f37'
